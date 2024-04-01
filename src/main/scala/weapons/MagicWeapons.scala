d7d77f0e033fe3913e11b3dc2f4477160af69e4b
DSanma <diego.sanmartin.a@ug.uchile.cl>
2024-04-01 18:57:46 -0300

    feat: TRAITS for weapons and magic weapons added

---
+++ src/main/scala/weapons/MagicWeapons.scala
@@ -0,0 +1,14 @@
+package weapons
+
+/**A trait that defines the characteristics of a magic weapon
+ *
+ * A magic weapon is defined by the charactersitics of a normal weapon and the magic attack points (magicattk)
+ *
+ * @tparam magicatk the magic attack points of a magic weapon
+ *
+ * @author Diego San Martin
+ */
+
+trait MagicWeapons extends Weapons {
+    def magicatk: Int
+}
---
+++ src/main/scala/weapons/Weapons.scala
@@ -0,0 +1,22 @@
+package weapons
+
+/** A trait used to define the characteristics of a weapon
+ *
+ * The charactersitics of a sword are defined by its name, attack points (atkpts), weight and owner
+ *
+ * @tparam name the name of the weapon
+ * @tparam atkpoints the attack points of the weapon
+ * @tparam weight the weight of the weapon (in kilos)
+ * @tparam owner the owner of the weapon
+ *
+ * @author Diego San Martin
+ */
+
+
+trait Weapons {
+  def name: String
+  def atkpoints: Int
+  def weight: Int
+  def owner: Option[Either[Either[Either[Paladin,Guerrero],Ninja],MagoNegro],MagoBlanco]
+
+}
