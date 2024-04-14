bb35d1172889a6dd935415956b2427091a450640
DSanma <diego.sanmartin.a@ug.uchile.cl>
2024-04-14 14:22:09 -0400

    feat: CLASS for the turn scheduler created

---
+++ src/main/scala/turnos/ProgramadorDeTurnos.scala
@@ -0,0 +1,14 @@
+package turnos
+
+import attributes.Attributes
+import enemigo.Enemigo
+
+import scala.collection.mutable.ArrayBuffer
+
+/** A class that calculates
+ *
+ */
+
+class ProgramadorDeTurnos (var players: ArrayBuffer[Attributes],var enemys: ArrayBuffer[Enemigo]){
+
+}
