fe2adbd13eee4f1d15a2d3a28362923add673ca4
DSanma <diego.sanmartin.a@ug.uchile.cl>
2024-04-23 11:43:27 -0400

    doc: added documentation for AEnemigo methods

--- src/main/scala/enemigo/AEnemigo.scala
+++ src/main/scala/enemigo/AEnemigo.scala
@@ -10,9 +10,19 @@ import entity.{AEntidad, Entidad}
 abstract class AEnemigo(name:String,hp:Int,defense:Int,weight:Int,private var attack:Int)
   extends AEntidad(name,hp,defense,weight) with EnemigoAttributes {
 
+  /**Getter for attack parameter of an enemy
+   *
+   * @return this.attack // the attack value of the enemy
+   */
   override def getAttack(): Int = {
     this.attack
   }
+
+  /**Method for attacking another entity
+   * Bare in mind, that an enemy can also inflict damage to another enemy
+   *
+   * @param victim the entity to whom damage will be dealt
+   */
 
   override def attack(victim: Entidad): Unit = {
     victim.takedamage(this)
