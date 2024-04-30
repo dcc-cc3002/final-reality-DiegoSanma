b7c46dabe20b1a4ba07b34675c60e2534eb58f8f
DSanma <diego.sanmartin.a@ug.uchile.cl>
2024-04-30 09:11:52 -0400

    feat: Added exceptions and documentation to both wepaon classes, magical and non magical

--- src/main/scala/weapons/Weapon.scala
+++ src/main/scala/weapons/Weapon.scala
@@ -1,8 +1,29 @@
 package weapons
 
 import attributes.Attributes
+import exceptions.{InvalidOwnerException, Require}
+
+/**Class for a weapon
+ * A weapon is defined by its name, attack points, weight and current owner
+ *
+ * A weapon is always created without an owner in the beginning, later one can be added
+ * Bare in mind that having an owner does not necessarily imply that said weapon is equipped/ being used by that owner
+ *
+ * @param name the name of the weapon
+ * @param atkpoints the attack points of the weapon
+ * @param weight the weight of the weapon
+ * @param owner the current owner of the weapon
+ *
+ * @author Diego San Martín
+ */
 
 class Weapon(name:String,atkpoints:Int,weight:Int,owner:Option[Attributes]=None)
   extends AWeapon(name,atkpoints,weight,owner) {
+
+  Require.Stat(atkpoints,"Attack points not valid") in (0 to 350)
+  Require.Stat(weight,"Weight not valid") in (0 to 150)
+  if(owner.isDefined){
+    throw new InvalidOwnerException("Weapon should not be initialized with an owner")
+  }
 
 }
