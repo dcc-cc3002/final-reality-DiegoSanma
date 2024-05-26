b829fdfe8da43de35d355e242a602ba1c15f094d
DSanma <diego.sanmartin.a@ug.uchile.cl>
2024-05-26 15:44:50 -0400

    feat: Extenden IMage and IHechizos traits with methods that will make it easier th throw exceptions when using different types of spells

--- src/main/scala/attributes/IMage.scala
+++ src/main/scala/attributes/IMage.scala
@@ -1,6 +1,14 @@
 package attributes
 
+import entity.Entidad
+import spells.IHechizo
+
 trait IMage extends Character{
   def getMana(): Unit
 
+  def throwSpell(spell:IHechizo)
+
+  def throwDarkSpell(spell:HechizoOscuro,victim:Entidad)
+
+  def throwLightSpell(spell:HechizoLuz,victim:Entidad)
 }
---
+++ src/main/scala/spells/HechizoOscuro.scala
@@ -0,0 +1,5 @@
+package spells
+
+class HechizoOscuro extends {
+
+}
