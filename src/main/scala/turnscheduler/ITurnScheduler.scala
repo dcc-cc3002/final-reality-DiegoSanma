f2a7258cdff64f4442f58033be9d069576e0a84c
DSanma <diego.sanmartin.a@ug.uchile.cl>
2024-06-03 17:32:46 -0400

    refactor: Removed files that were copies of the spanish versions of classes2

--- src/main/scala/turnscheduler/ITurnScheduler.scala
+++ src/main/scala/turnscheduler/ITurnScheduler.scala
@@ -14,6 +14,7 @@ import scala.collection.mutable.ArrayBuffer
 trait ITurnScheduler {
 
   def getTurnsLine(): ArrayBuffer[Entity]
+
   def addTo(added:Entity): Unit
 
   def addCharacter(added:Attributes): Unit
